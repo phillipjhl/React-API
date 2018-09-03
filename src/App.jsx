58bf1d84ebac255113f8791bf07d9ff6760b2648
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-03 10:46:28 -0500

    PHASE 2 Complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ import FilmCard from './components/FilmCard';
 class App extends Component {
     constructor(props) {
         super(props);
+        
         this.state = {
-            loaded: true
+            loaded: false
         };
+
+        this.handleLoadFilms = this.handleLoadFilms.bind(this);
     }
 
     componentDidMount() {
@@ -26,13 +29,28 @@ class App extends Component {
             });
 
     }
+
+    handleLoadFilms() {
+        this.setState({ loaded: true })
+    }
+
     render() {
+        if (this.state.loaded) {
+            return (
+                <Fragment>
+                    {this.state.filmCards}
+                </Fragment>
+            );
+        } else {
         return (
-            <Fragment>
-                { this.state.filmCards }
-            </Fragment>
-        );
+            <div className="jumbotron">
+                <img className="mw-100 " alt="Studio Ghibli" src="https://ghibliapi.herokuapp.com/images/logo.svg" ></img>
+                <hr className="my-4" />
+                <button className="btn btn-dark btn-lg" onClick= {this.handleLoadFilms}>Load Flms</button>
+            </div>
+            );
+        }
     }
 }
-
+            
 export default App;
